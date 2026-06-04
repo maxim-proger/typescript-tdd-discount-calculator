3bf2f8afde83a5d392f665100d232db659c33cda
maxim-proger <ml-design@mail.ru>
2026-06-04 20:55:23 +0300

    feat: add maximum discount cap

--- src/discount.ts
+++ src/discount.ts
@@ -18,7 +18,5 @@ export function calculateDiscount(orderAmount: number, isVip: boolean): number {
         discount += DISCOUNT_VIP
     }
 
-
-
-    return discount
+    return Math.min(discount, DISCOUNT_MAX)
 }
